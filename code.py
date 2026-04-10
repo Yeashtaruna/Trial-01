8c93f8b8f48f8192688ed0b1e027c7b6f242b60e
Yeashtaruna Zeem <yeashtaruna23@gmail.com>
2026-04-10 17:44:19 +0600

    updated

--- code.py
+++ code.py
@@ -1,10 +1,9 @@
 print("hello world")
 def add(a, b):
     return a + b
-def subtract(a, b):
-    return a - b
+
 add_result = add(5, 3)
-subtract_result = subtract(5, 3)
+
 print("Addition Result:", add_result)
-print("Subtraction Result:", subtract_result)
+
 print("Made changes")
